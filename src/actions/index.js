074ffceaa5af206f409addcffd9a01d86be54009
Peter <peter.prentiss@gmail.com>
2017-12-14 15:48:10 -0600

    Create authReducer.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import history from "../utils/historyUtils";
+import { AUTH_USER } from './types';
 
 const ROOT_URL = 'http://localhost:3090';
 
@@ -7,6 +8,8 @@ export function signInUser({ email, password }) {
     return function(dispatch) {
         axios.post(`${ROOT_URL}/signin`, { email, password })
             .then(response => {
+                dispatch({ type: AUTH_USER });
+
                 history.push('/feature');
             })
             .catch(() => {
