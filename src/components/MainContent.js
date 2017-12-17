1cf1c33d08eed01a8875753c080dd483b3af86ea
Peter <peter.prentiss@gmail.com>
2017-12-16 21:21:27 -0600

    Create signout component.

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import Signin from './auth/Signin';
+import Signout from './auth/Signout';
 
 export default class MainContent extends Component {
     render() {
@@ -9,6 +10,7 @@ export default class MainContent extends Component {
             <div className="container">
                 <Switch>
                     <Route path="/signin" component={Signin} />
+                    <Route path="/signout" component={Signout} />
                 </Switch>
             </div>
         )
