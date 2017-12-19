e4799d11e3cc972aad87471406bf410c6c985c6b
Peter <peter.prentiss@gmail.com>
2017-12-19 16:01:48 -0600

    Bugs.

--- src/components/auth/Signup.js
+++ src/components/auth/Signup.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { reduxForm } from 'redux-form';
 import * as actions from '../../actions';
 
+
 class Signup extends Component {
     render() {
         const { handleSubmit, fields: { email, password, passwordConfirm }} = this.props;
@@ -15,9 +16,10 @@ class Signup extends Component {
                 <fieldset className="form-group">
                     <label>Password:</label>
                     <input className="form-control" {...password} type="password"/>
+                    {password.touched && password.error && <div className="error">{password.error}</div>}
                 </fieldset>
                 <fieldset className="form-group">
-                    <label>ConfirmPassword:</label>
+                    <label>Confirm Password:</label>
                     <input className="form-control" {...passwordConfirm} type="password"/>
                 </fieldset>
                 <button action="submit" className="btn btn-primary">Sign Up</button>
@@ -26,7 +28,27 @@ class Signup extends Component {
     }
 }
 
+// function validate(formProps) {
+//     const errors = {};
+
+//     if (formProps.password !== formProps.passwordConfirm) {
+//         errors.password = 'Passwords must match';
+//     }
+
+//     return errors;
+// }
+
+const validateForm = values => {
+    const errors = {};
+    const { password, passwordConfirm } = values;
+    if (password !== passwordConfirm) {
+        errors.passwordConfirm = "Password does not match."
+    }
+    return errors;
+};
+
 export default reduxForm({
     form: 'signup',
-    fields: ['email, password', 'passwordConfirm']
+    fields: ['email, password', 'passwordConfirm'],
+    validate: validateForm
 })(Signup);
